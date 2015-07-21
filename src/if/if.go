2626e218114f1053d63f48502d04c5f79afb549a
Dan <danv@cstructor.com>
2015-07-21 07:50:05 -0700

    fix if

--- src/if/if.go
+++ src/if/if.go
@@ -5,17 +5,17 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i%2 == 0 {
-			fmt.Println(i)
-		}
+	i := 3
+
+	if i%2 == 0 {
+		fmt.Println(i)
 	}
 
 	if t := 44; t%2 == 0 {
 		fmt.Println(t)
 	}
 
-	i := 2
+	i = 2
 	if i == 2 {
 		fmt.Println(2)
 	} else if i%3 == 0 {
