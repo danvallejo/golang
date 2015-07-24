4bfc9ae1605070d71ef34adaaa16dfb327cd5422
Vallejo, Dan <Dan.Vallejo@exchange.nordstrom.com>
2015-07-24 13:35:53 -0700

    upd restservice

--- src/restservice/service.go
+++ src/restservice/service.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
-import "encoding/json"
-import "container/list"
-import "github.com/go-martini/martini"
+import (
+	"container/list"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-martini/martini"
+)
 
 type Contact struct {
-	Name        string
-	PhoneNumber string
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	PhoneNumber string `json:phone_number`
 }
 
 type Contacts list.List
@@ -25,7 +31,18 @@ func listToContactSlice(list list.List) []Contact {
 	return arr
 }
 
-func get() string {
+func getElement(id int, list list.List) *Contact {
+	for element := contacts.Front(); element != nil; element = element.Next() {
+		contact := element.Value.(Contact)
+		if contact.Id == id {
+			return &contact
+		}
+	}
+
+	return nil
+}
+
+func getContacts() string {
 	arr := listToContactSlice(contacts)
 
 	jsonBytes, _ := json.Marshal(arr)
@@ -34,6 +51,35 @@ func get() string {
 	fmt.Println(string(jsonBytes))
 
 	return string(jsonBytes)
+}
+
+func getSpecific(parms martini.Params) (int, string) {
+	id, _ := strconv.Atoi(parms["id"])
+
+	contact := getElement(id, contacts)
+	if contact == nil {
+		return http.StatusNotFound, ""
+	}
+
+	jsonBytes, _ := json.Marshal(contact)
+
+	fmt.Println(*contact)
+	fmt.Println(string(jsonBytes))
+
+	return http.StatusOK, string(jsonBytes)
+}
+
+func postContact(w http.ResponseWriter, r *http.Request) (int, string) {
+	contact := getPostContact(r)
+	contacts.PushBack(*contact)
+	return http.StatusCreated, "created"
+}
+
+func getPostContact(r *http.Request) *Contact {
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	name := r.FormValue("name")
+	phoneNumber := r.FormValue("phone_number")
+	return &Contact{Id: id, Name: name, PhoneNumber: phoneNumber}
 }
 
 func notfound() string {
@@ -45,13 +91,13 @@ func notfound() string {
 func main() {
 	m := martini.Classic()
 
-	contacts.PushBack(Contact{Name: "mike", PhoneNumber: "111"})
-	contacts.PushBack(Contact{Name: "dan", PhoneNumber: "222"})
+	contacts.PushBack(Contact{Id: 2, Name: "mike", PhoneNumber: "111"})
+	contacts.PushBack(Contact{Id: 1, Name: "dan", PhoneNumber: "222"})
 
-	fmt.Println(contacts)
-
-	m.Get("/contacts", get)
 	m.NotFound(notfound)
+	m.Get("/contacts", getContacts)
+	m.Get("/contacts/:id", getSpecific)
+	m.Post("/contacts", postContact)
 
 	m.Run()
 }
