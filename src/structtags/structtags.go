c7d7d57f22b748cf93da117dda84a46fc57d0f1e
Dan <danv@cstructor.com>
2015-07-29 18:51:51 -0700

    upd

--- src/structtags/structtags.go
+++ src/structtags/structtags.go
@@ -6,13 +6,19 @@ import (
 )
 
 func main() {
-	type S struct {
-		F string `species:"gopher" color:"blue"`
+
+	type Animal struct {
+		Name string `species:"gopher" color:"blue"`
 	}
 
-	s := S{}
-	st := reflect.TypeOf(s)
+	animal := Animal{}
+
+	st := reflect.TypeOf(animal)
+	fmt.Println("st=",st)
+
 	field := st.Field(0)
+	fmt.Println("field=", field)
+	fmt.Println("field.Tag=",field.Tag)
 
 	fmt.Println(field.Tag.Get("color"), field.Tag.Get("species"))
 }
