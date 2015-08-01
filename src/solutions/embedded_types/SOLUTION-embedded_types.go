5a63828c5647f021ed106a79f638449fe33ddd73
dan <danv@cstructor.com>
2015-08-01 10:17:58 -0700

    rename to SOLUTION-embedded_types

--- src/solutions/embedded_types/SOLUTION-embedded_types.go
+++ src/solutions/embedded_types/SOLUTION-embedded_types.go
@@ -11,7 +11,11 @@ type Circle struct {
 	Radius int
 }
 
-func (this Circle) Print() {
+func (this Shape) Print(){
+	fmt.Println(this.Name)
+}
+
+func (this Circle) xPrint() {
 	fmt.Println(this.Name, this.Radius)
 }
 
@@ -19,4 +23,18 @@ func main() {
 	circle := Circle{Shape: Shape{Name: "circle"}, Radius: 22}
 
 	circle.Print()
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
